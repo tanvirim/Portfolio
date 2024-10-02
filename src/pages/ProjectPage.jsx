0724322100b1d6aad6b4b6b68afc2c5c1c55bf5f
tanvirmitul1 <mitulseopage1@gmail.com>
2024-10-02 16:24:03 +0600

    back button add

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import ProjectCards from "../components/project/Project";
 
 const ProjectPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center ">
       <div className="text-center">
@@ -8,6 +10,13 @@ const ProjectPage = () => {
           This Page is Under Construction
         </p>
         <ProjectCards />
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </button>
       </div>
     </div>
   );
